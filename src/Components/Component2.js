71a5fcdc391fff07a130b4a7a08525002c18ad06
ITaxu Data <99535242+itaxudata@users.noreply.github.com>
2025-01-23 15:56:13 +0100

    Update Component2.js

--- src/Components/Component2.js
+++ src/Components/Component2.js
@@ -6,7 +6,8 @@ const Component2 = () => {
     <div className='main-2-div'>
         <h1 className='h1-2'>COMPONENT - 2</h1>
         <br>
-        <a href="https://itaxudata.se">"itaxudata.se"</a>
+        <a href="https://itaxudata.se" target="_blank" rel="noopener noreferrer">
+      itaxudata.se</a>
     </div>
   )
 }
